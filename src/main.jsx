40734f2af0c1fddf722de9d107dd60435c9c3acb
kacper32332 <01187372@pw.edu.pl>
2026-04-01 11:34:46 +0200

    change file structure

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import './styles/index.css';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <App />
+  </GoogleOAuthProvider>
+);
